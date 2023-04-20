1c7a6afdf5fc6276184630a73cea78583e68dcf6
Orwa Diraneyya <orwa.diraneyya@wildcodeschool.com>
2023-04-20 09:09:13 +0200

    add rome as linter/formatter

--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
-import express, { Express, Request, Response } from 'express';
-import dotenv from 'dotenv';
+import express, { Express, Request, Response } from "express";
+import expressOasGenerator from "express-oas-generator";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const app: Express = express();
+expressOasGenerator.init(app, {});
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server. This is cool 😬');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Express + TypeScript Server. This is cool 😬");
+});
+
+app.delete("/kill_the_witch", (req: Request, res: Response) => {
+  res.sendStatus(501);
 });
 
 app.listen(port, () => {
