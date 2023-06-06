30aba59a533bcc0e58c6f72d4b6d3fbeadb9847a
George Surmava <111701072+surmavagit@users.noreply.github.com>
2023-06-06 12:02:04 +0200

    test refactoring

--- packages/test-server/test/api.test.ts
+++ packages/test-server/test/api.test.ts
@@ -1,57 +1,56 @@
 import { afterAll, expect, test, expectTypeOf } from "vitest";
-import { app } from "server";
 import supertest from "supertest";
-import * as http from "http";
+import { server } from "../testserver";
 // import { ResponseGetTablesMine } from 'prisma-queries';
 
 test("root", async () => {
-  const root = await supertest(http.createServer(app)).get("/").expect(200);
+  const root = await supertest(server).get("/").expect(200);
 })
 
 test('menu', async () => {
-  const menu = await supertest(http.createServer(app))
+  const menu = await supertest(server)
     .get("/api/menu")
     .expect(200);
   })
 
   test('tables', async () => {
-  const tables = await supertest(http.createServer(app))
+  const tables = await supertest(server)
     .get("/api/tables")
     .expect(200);
   })
 
   test('login', async () => {
-  const login = await supertest(http.createServer(app))
+  const login = await supertest(server)
     .get("/api/login")
     .expect(401);
   })
 
   test('loginPOSTnoBody', async () => {
-  const loginPOSTnoBody = await supertest(http.createServer(app))
+  const loginPOSTnoBody = await supertest(server)
     .post("/api/login")
     .expect(400);
   })
 
   test('tablesMine', async () => {
-  const tablesMine = await supertest(http.createServer(app))
+  const tablesMine = await supertest(server)
     .get("/api/tables/mine")
     .expect(401);
   })
 
   test('tablesById', async () => {
-  const tablesById = await supertest(http.createServer(app))
+  const tablesById = await supertest(server)
     .get("/api/tables/5")
     .expect(401);
   })
 
   test('tablesByIdPOST', async () => {
-  const tablesByIdPOST = await supertest(http.createServer(app))
+  const tablesByIdPOST = await supertest(server)
     .post("/api/tables/5")
     .expect(401);
   })
 
   test('tablesByIdPUT', async () => {
-  const tablesByIdPUT = await supertest(http.createServer(app))
+  const tablesByIdPUT = await supertest(server)
     .put("/api/tables/5")
     .expect(401);
 });
